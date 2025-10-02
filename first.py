20657f86e1d6e07ffdf32a830c1a32fad3888e1e
jiriherza <jiriherza@seznam.cz>
2025-10-02 12:19:21 +0000

    úkol1

---
+++ first.py
@@ -0,0 +1,9 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        print(f"{cislo} je sudé číslo")
+    else:
+        print(f"{cislo} je liché číslo")
+
+
+(sudy_nebo_lichy(5))
+(sudy_nebo_lichy(1000000))
